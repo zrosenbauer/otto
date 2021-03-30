106a6a07afd9c1a826d2d283101830011afee382
Seth Rosenbauer <seth@bluenova.io>
2021-03-29 19:47:41 -0500

    BN-517 | Initial CLI tool (#1)
    * changes for cli tool
    
    * cleaning up example cli command
    
    * linting changes
    
    * no longer needed
    
    * more updates
    
    * Delete package-lock.json
    
    * adding yarn lock
    
    Co-authored-by: Zac Rosenbauer <zac@bluenova.io>

---
+++ dist/commands/example.d.ts
@@ -0,0 +1,4 @@
+export declare const command = "example";
+export declare const describe = "this is an example cli command";
+export declare const builder: {};
+export declare function handler(argv: any): Promise<void>;
---
+++ src/cli.ts
@@ -0,0 +1,32 @@
+import * as path from 'path';
+import * as cli from 'yargs';
+
+(async () => {
+
+  // eslint-disable-next-line no-unused-expressions
+  cli
+    .scriptName('otto')
+    .commandDir(path.join(__dirname, 'commands'), {
+      extensions: ['ts', 'js'],
+      exclude: /\.d\.ts$/
+    })
+    .command({
+      command: '*',
+      handler() {
+        console.log(
+          'Argument required, please pass in a proper argument or use --help if you need help'
+        );
+      }
+    })
+    .option('debug', {
+      alias: 'd',
+      describe: 'Verbose logging for debugging'
+    })
+    .fail((msg, err, yargs) => {
+      console.log(`ERROR: ${msg || err.message}`);
+      process.exit(1);
+    })
+    .wrap(cli.terminalWidth() * 0.8)
+    .help()
+    .showHelpOnFail(false).argv;
+})();
